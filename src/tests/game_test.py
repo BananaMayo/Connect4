1a03932223610d85553702d24f87aed62cdfef86
BananaMayo <niklas.nygard01@gmail.com>
2022-11-11 12:26:47 +0200

    testien aloittelua

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -1,24 +1,35 @@
 import unittest
-import pygame
-import sys
-from alustava_pelipohja import pelilauta
+#import pygame
+#import sys
+#from alustava_pelipohja import Pelin_alustukset
+from alustava_pelipohja import Nakyma
+#import alustava_pelipohja as ap
 
-class TestPelaaja(unittest.TestCase):
+RIVIT = 6
+SARAKKEET = 7
+NELION_KOKO = 100
+PELAAJAN_KIEKKO = 1
+AI_KIEKKO = 2
+
+class TestPeli(unittest.TestCase):
     def setUp(self):
-        self.click = False
-        pygame.init()
+        #pygame.init()
         self.PELAAJAN_KIEKKO = 1
         self.PELAAJA = 0
-        self.pelilauta = pelilauta()
+        #self.pelilauta = Pelin_alustukset()
         
-    def test_pelaajan_kiekko(self):
+    """ def test_pelaajan_kiekko(self):
         if self.pelilauta[self.rivi][self.sarake] == self.PELAAJAN_KIEKKO:
             self.assertEqual(pygame.draw.circle(self.naytto, self.KELTAINEN, (int(self.sarake*self.NELION_KOKO+self.NELION_KOKO/2), self.korkeus-int(self.rivi*self.NELION_KOKO+self.NELION_KOKO/2)), self.SADE))
-
-
-"""     def test_main_to_game(self):
-        mx, my = pygame.mouse.get_pos()
-        self.game1_button = pygame.Rect(245, 120, 150, 30)
-        if self.game1_button.collidepoint((mx, my)):
-            if self.click:
-               self.assertEqual(self.labyrinth.on_execute()) """
+ """
+    """ def test_seuraava_avoin_rivi(self):
+        avoin_rivi = self.pelilauta.seuraava_avoin_rivi(0,0)
+        for rivi in avoin_rivi:
+            self.assertEqual(avoin_rivi, rivi) """
+    
+    def test_nakyma(self):
+        kiekko = 4
+        näkymä = 0
+        n = Nakyma._näkymä(näkymä, kiekko)
+        n.count(kiekko)
+        self.assertEqual(Nakyma.tulos, 100)
