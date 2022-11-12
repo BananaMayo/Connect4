ca4999f4cf19bc32f132acefa9d834b9d3543014
BananaMayo <niklas.nygard01@gmail.com>
2022-11-12 11:58:01 +0200

    testien tekoa, Nakyman testaus

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -1,9 +1,5 @@
 import unittest
-#import pygame
-#import sys
-#from alustava_pelipohja import Pelin_alustukset
-from alustava_pelipohja import Nakyma
-#import alustava_pelipohja as ap
+from luokka_nakyma import Nakyma
 
 RIVIT = 6
 SARAKKEET = 7
@@ -11,25 +7,22 @@ NELION_KOKO = 100
 PELAAJAN_KIEKKO = 1
 AI_KIEKKO = 2
 
-class TestPeli(unittest.TestCase):
+class TestNakyma(unittest.TestCase):
     def setUp(self):
-        #pygame.init()
         self.PELAAJAN_KIEKKO = 1
         self.PELAAJA = 0
-        #self.pelilauta = Pelin_alustukset()
+        self.näkymä = Nakyma(0)
         
-    """ def test_pelaajan_kiekko(self):
-        if self.pelilauta[self.rivi][self.sarake] == self.PELAAJAN_KIEKKO:
-            self.assertEqual(pygame.draw.circle(self.naytto, self.KELTAINEN, (int(self.sarake*self.NELION_KOKO+self.NELION_KOKO/2), self.korkeus-int(self.rivi*self.NELION_KOKO+self.NELION_KOKO/2)), self.SADE))
- """
-    """ def test_seuraava_avoin_rivi(self):
-        avoin_rivi = self.pelilauta.seuraava_avoin_rivi(0,0)
-        for rivi in avoin_rivi:
-            self.assertEqual(avoin_rivi, rivi) """
-    
     def test_nakyma(self):
-        kiekko = 4
-        näkymä = 0
-        n = Nakyma._näkymä(näkymä, kiekko)
-        n.count(kiekko)
-        self.assertEqual(Nakyma.tulos, 100)
+        näkymä = []
+        a = Nakyma._näkymä(1,näkymä,4)
+        self.assertEqual(a, 100)
+
+""" def test_pelaajan_kiekko(self):
+    if self.pelilauta[self.rivi][self.sarake] == self.PELAAJAN_KIEKKO:
+        self.assertEqual(pygame.draw.circle(self.naytto, self.KELTAINEN, (int(self.sarake*self.NELION_KOKO+self.NELION_KOKO/2), self.korkeus-int(self.rivi*self.NELION_KOKO+self.NELION_KOKO/2)), self.SADE))
+"""
+""" def test_seuraava_avoin_rivi(self):
+    avoin_rivi = self.pelilauta.seuraava_avoin_rivi(0,0)
+    for rivi in avoin_rivi:
+        self.assertEqual(avoin_rivi, rivi) """
