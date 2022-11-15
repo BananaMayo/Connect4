345eafde4398f75a4b754a6839d857daae45a510
BananaMayo <niklas.nygard01@gmail.com>
2022-11-15 10:49:42 +0200

    muutama testi Näkymä-luokkaa varten

--- src/luokka_nakyma.py
+++ src/luokka_nakyma.py
@@ -4,20 +4,18 @@ AI_KIEKKO = 2
 TYHJA = 0
 
 class Nakyma:
-    def __init__(self, tulos = 0):
-        self.tulos  = tulos
-
-    def _näkymä(self, näkymä, kiekko):
+    def _näkymä(näkymä, kiekko):
+        tulos = 0
         vastustajan_kiekko = PELAAJAN_KIEKKO
         if kiekko == PELAAJAN_KIEKKO:
             vastustajan_kiekko == AI_KIEKKO
  
         if näkymä.count(kiekko) == 4:
-            self.tulos += 100
+            tulos += 100
         elif näkymä.count(kiekko) == 3 and näkymä.count(TYHJA) == 1:
-            self.tulos += 5
+            tulos += 5
         elif näkymä.count(kiekko) == 2 and näkymä.count(TYHJA) == 2:
-            self.tulos += 2
+            tulos += 2
         if näkymä.count(vastustajan_kiekko) == 3 and näkymä.count(TYHJA) == 1:
-            self.tulos -= 4
-        return self.tulos
+            tulos -= 4
+        return tulos
--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -11,18 +11,32 @@ class TestNakyma(unittest.TestCase):
     def setUp(self):
         self.PELAAJAN_KIEKKO = 1
         self.PELAAJA = 0
-        self.näkymä = Nakyma(0)
+        self.näkymä = []
+        self.kiekko = 1
+        Nakyma._näkymä(self.näkymä, self.kiekko)
 
-    def test_nakyma(self):
-        näkymä = []
-        a = Nakyma._näkymä(1,näkymä,4)
-        self.assertEqual(a, 100)
+    def test_nakyma_count(self):
+        näkymä = [1,1,1,1]
+        kiekko = 1
+        Nakyma._näkymä(näkymä,kiekko)
+        a = näkymä.count(kiekko)
+        self.assertEqual(a, 4)
 
-""" def test_pelaajan_kiekko(self):
-    if self.pelilauta[self.rivi][self.sarake] == self.PELAAJAN_KIEKKO:
-        self.assertEqual(pygame.draw.circle(self.naytto, self.KELTAINEN, (int(self.sarake*self.NELION_KOKO+self.NELION_KOKO/2), self.korkeus-int(self.rivi*self.NELION_KOKO+self.NELION_KOKO/2)), self.SADE))
-"""
-""" def test_seuraava_avoin_rivi(self):
-    avoin_rivi = self.pelilauta.seuraava_avoin_rivi(0,0)
-    for rivi in avoin_rivi:
-        self.assertEqual(avoin_rivi, rivi) """
+    def test_nakyma_4(self):
+        näkymä = [1,1,1,1]
+        kiekko = 1
+        c = Nakyma._näkymä(näkymä,kiekko)
+        self.assertEqual(c, 100)
+
+    def test_nakyma_3(self):
+        näkymä = [1,1,1,0]
+        kiekko = 1
+        tyhja = 0
+        b = Nakyma._näkymä(näkymä, kiekko)
+        self.assertEqual(b, 1)
+    
+    def test_nakyma_2(self):
+        näkymä = [1,1,0,0]
+        kiekko = 1
+        d = Nakyma._näkymä(näkymä,kiekko)
+        self.assertEqual(d, 2)
