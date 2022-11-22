d2cc470789510d6c972306769097806b5a40fc7b
BananaMayo <niklas.nygard01@gmail.com>
2022-11-22 16:50:11 +0200

    Kommentointia lisätty

--- src/luokka_ratkaiseva_sijotus.py
+++ src/luokka_ratkaiseva_sijotus.py
@@ -7,22 +7,22 @@ IKKUNA = 4
 class RatkaisevaSijotus:
     #pylint: disable=unsubscriptable-object
     #pylint: disable=pointless-string-statement
+    """Luokan funktio tarkistaa erikseen neljän suoran laudalla, sisältää 
+    pysty-, vaaka- ja digaonaaliset suunnat. Mikäli neljän neljän suora on saavutettu
+    palautetaan arvo 'True'
+    """
     def ratkaiseva_sijotus(pelilauta, kiekko):
-
-        """Vaakasuorassa saatu neljän suoran tarkistus:
-        """
+        # Vaakasuorassa saatu neljän suoran tarkistus:
         for sarake in range(SARAKKEET-3):
             for rivi in range(RIVIT):
                 if pelilauta[rivi][sarake] == kiekko and pelilauta[rivi][sarake+1] == kiekko and pelilauta[rivi][sarake+2] == kiekko and pelilauta[rivi][sarake+3]==kiekko:
                     return True
-        """Pystysuorassa saatu neöjän suoran tarkistus:
-        """
+        # Pystysuorassa saatu neljän suoran tarkistus:
         for sarake in range(SARAKKEET):
             for rivi in range(RIVIT-3):
                 if pelilauta[rivi][sarake] == kiekko and pelilauta[rivi+1][sarake] == kiekko and pelilauta[rivi+2][sarake] == kiekko and pelilauta[rivi+3][sarake]==kiekko:
                     return True
-        """Diagonaalisessa suunnassa saatu neljän suora:
-        """
+        # Diagonaalisessa suunnassa saatu neljän suora:
         for sarake in range(SARAKKEET-3):
             for rivi in range(RIVIT-3):
                 if pelilauta[rivi][sarake] == kiekko and pelilauta[rivi+1][sarake+1] == kiekko and pelilauta[rivi+2][sarake+2] == kiekko and pelilauta[rivi+3][sarake+3]==kiekko:
