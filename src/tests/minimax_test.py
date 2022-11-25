12b4e193456dfdd4b66a5ef6aff745139c7ee54a
BananaMayo <niklas.nygard01@gmail.com>
2022-11-25 16:46:01 +0200

    testiä

--- src/tests/minimax_test.py
+++ src/tests/minimax_test.py
@@ -1,5 +1,6 @@
 import unittest
 import math
+import pygame
 from pelipohja import MiniMax, tulos
 
 RIVIT = 6
@@ -10,10 +11,12 @@ AI_KIEKKO = 2
 
 class TestMinimax (unittest.TestCase):
     def setup(self):
+
         self.alpha = max()
         self.beta = min()
 
     def test_minimax_ai_voitto(self):
+        pygame.init()
         minimax = MiniMax
         syvyys = 5
         pelilauta = [[0, 0, 0, 0, 0, 0, 0,],
@@ -28,6 +31,7 @@ class TestMinimax (unittest.TestCase):
         self.assertEqual(a[1], ai_pisteet)
     
     def test_ai_havio(self):
+        pygame.init()
         self.minimax = MiniMax
         syvyys = 5
         pelilauta = [[0, 0, 0, 0, 0, 0, 0,],
