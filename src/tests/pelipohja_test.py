f6b87b125dfbc038f5860bbf807c32e4a857999e
BananaMayo <niklas.nygard01@gmail.com>
2022-12-01 21:18:17 +0200

    pelipohja.py:n testaaminen ei onnistu

--- src/tests/pelipohja_test.py
+++ src/tests/pelipohja_test.py
@@ -1,6 +1,9 @@
 import unittest
 #from pelipohja import pe
 
+#Tämän luokan testaaminen ei oikein onnistu
+#koska luokan importtaaminen käynnistää pygamen
+#joka ei toimi CI-pipelinessä
 class TestPelipohja(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
