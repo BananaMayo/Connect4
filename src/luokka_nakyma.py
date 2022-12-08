1878a30bf29eca599354187b5d207c4f90452606
BananaMayo <niklas.nygard01@gmail.com>
2022-12-08 09:59:14 +0200

    pylint virheiden korjausta

--- src/luokka_nakyma.py
+++ src/luokka_nakyma.py
@@ -3,6 +3,7 @@ PELAAJAN_KIEKKO = 1
 AI_KIEKKO = 2
 TYHJA = 0
 
+
 class Nakyma:
     """Luokassa käydään läpi siirroista saadut pisteet. Kuten esim.
     jos pelaajalla on kaksi peräkkäistä kiekkoa, niin kolmannen kiekon
@@ -10,18 +11,18 @@ class Nakyma:
     """
     #pylint: disable=no-self-argument
     #pylint: disable=no-member
-    def nakyma_(näkymä, kiekko):
+    def nakyma_(nakyma, kiekko):
         tulos = 0
         vastustajan_kiekko = PELAAJAN_KIEKKO
         if kiekko == PELAAJAN_KIEKKO:
             vastustajan_kiekko == AI_KIEKKO
             tulos += 0.0
-        if näkymä.count(kiekko) == 4:
+        if nakyma.count(kiekko) == 4:
             tulos += 100
-        elif näkymä.count(kiekko) == 3 and näkymä.count(TYHJA) == 1:
+        elif nakyma.count(kiekko) == 3 and nakyma.count(TYHJA) == 1:
             tulos += 5
-        elif näkymä.count(kiekko) == 2 and näkymä.count(TYHJA) == 2:
+        elif nakyma.count(kiekko) == 2 and nakyma.count(TYHJA) == 2:
             tulos += 2
-        if näkymä.count(vastustajan_kiekko) == 3 and näkymä.count(TYHJA) == 1:
+        if nakyma.count(vastustajan_kiekko) == 3 and nakyma.count(TYHJA) == 1:
             tulos -= 4
         return tulos
